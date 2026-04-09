3482bdf2d3b6a1de6bd7941280b78908a412f6b4
DesPeruRest <qrqdsq45@gmail.com>
2026-04-09 08:49:28 -0500

    Cambios para static app service

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,15 +1,18 @@
 import { API_BASE_URL } from '../config/constants'
 
-const API_ORIGIN = new URL(API_BASE_URL).origin
+const API_ORIGIN = window.location.origin
 
 export function resolveMediaUrl(maybeRelativeUrl) {
   if (!maybeRelativeUrl) return ''
   if (typeof maybeRelativeUrl !== 'string') return ''
+
   if (maybeRelativeUrl.startsWith('http://') || maybeRelativeUrl.startsWith('https://')) {
     return maybeRelativeUrl
   }
+
   if (maybeRelativeUrl.startsWith('/')) {
     return `${API_ORIGIN}${maybeRelativeUrl}`
   }
+
   return `${API_ORIGIN}/${maybeRelativeUrl}`
 }
